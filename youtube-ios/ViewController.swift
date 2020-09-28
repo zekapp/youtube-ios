347457f048e9d1b6ad55fdfbbccbbd27e6835f0f
zeki guler <zeki.guler@medibank.com.au>
2020-09-28 15:08:22 +1000

    initial xcode project creation

---
+++ youtube-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-ios
+//
+//  Created by Zeki Guler on 28/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
