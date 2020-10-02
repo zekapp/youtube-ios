0aa2cc133a05311a7a93e6705b8dddf354d6bde3
zeki guler <zeki.guler@medibank.com.au>
2020-10-02 13:43:28 +1000

    con

--- youtube-ios/Model.swift
+++ youtube-ios/Model.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+class Model {
+    func getVideos() {
+        
+        // create a URL object
+        let url = URL(string:  Constants.API_URL)
+        
+        guard url != nil else {
+            return
+        }
+        
+        let session = URLSession.shared
+        
+        let dataTask = session.dataTask(with: url!) { (data, response, error) in
+            
+            // check if there were any errors
+            if error != nil || data == nil {
+                return
+            }
+            
+            // parsing
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                let response = try decoder.decode(Response.self, from: data!)
+                dump(response)
+            } catch {
+                
+            }
+            
+            
+        }
+        
+        dataTask.resume()
+        
+    }
+}
--- youtube-ios/ViewController.swift
+++ youtube-ios/ViewController.swift
@@ -8,14 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var model = Model()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        var i = 0
+        model.getVideos()
     }
-
-
+    
+    
 }
 
